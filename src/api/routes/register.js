cbc037ac9b87eda0fc77faebdf253688e9972e88
LiamBinedell <2574850@students.wits.ac.za>
2024-04-30 20:05:26 +0200

    made register.js do literally nothing

--- src/api/routes/register.js
+++ src/api/routes/register.js
@@ -5,11 +5,7 @@ const router = express.Router();
 router.post('/', (req, res) =>{
     //const {name, surname, email, password, role, company} = req.body;
     //firebaseApp.registerUser(name, surname, email, password, role, company);
-    try {
-        res.send("You've reached the register api");
-    } catch (error) {
-        res.send("ERROR:", error);
-    }
+    //res.send("You've reached the register api");
 });
 
 module.exports = router;
