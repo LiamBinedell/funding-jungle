b7bfdd28aa40141fd35f47099495792b60fc7fb9
LiamBinedell <2574850@students.wits.ac.za>
2024-04-30 19:57:56 +0200

    added try/catch to register.js

--- src/api/routes/register.js
+++ src/api/routes/register.js
@@ -5,7 +5,11 @@ const router = express.Router();
 router.post('/', (req, res) =>{
     //const {name, surname, email, password, role, company} = req.body;
     //firebaseApp.registerUser(name, surname, email, password, role, company);
-    res.send("You've reached the register api");
+    try {
+        res.send("You've reached the register api");
+    } catch (error) {
+        res.send("ERROR:", error);
+    }
 });
 
 module.exports = router;
