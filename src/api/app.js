018a191067431a030b3b55dc3943cf099ba78508
LiamBinedell <2574850@students.wits.ac.za>
2024-04-30 20:08:12 +0200

    made app.js completely ignore register.js

--- src/api/app.js
+++ src/api/app.js
@@ -5,7 +5,11 @@ const registerRouter = require('./routes/register');
 
 //app.use(express.static(__dirname));
 app.use(cors());
-app.use('/api/register', registerRouter);
+//app.use('/api/register', registerRouter);
+
+app.post('/api/register/', (req, res) => {
+
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
