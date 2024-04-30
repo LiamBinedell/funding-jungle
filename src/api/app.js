d1e42c7c75f25c4bc295d5f5ad96b229214c5e7d
LiamBinedell <2574850@students.wits.ac.za>
2024-04-30 19:10:26 +0200

    added cors dependency to avoid cross origin issues

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const registerRouter = require('./routes/register');
 
 //app.use(express.static(__dirname));
+app.use(cors());
 app.use('/api/register', registerRouter);
 
 const PORT = 3000;
