3a53bbcd266f23ee4f4be730214d095f7951d699
LiamBinedell <2574850@students.wits.ac.za>
2024-05-01 14:27:02 +0200

    added admin page and logic to remove existing ads. Waiting on database to be able to get existing ads from firebase and other api logic

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 //import routers
 const registerRouter = require('./api/routes/register');
+const adminRouter = require('./api/routes/admin');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(cors());
 
 //set routes
 app.use('/api/register', registerRouter);
+app.use('/api/admin', adminRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "components", "index.html"));
--- src/javascript/admin.js
+++ src/javascript/admin.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     
+    
+    const buttons = document.querySelectorAll(".removeButton");
+
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            const listItem = this.closest(".listElement");
+            if (listItem){
+                listItem.remove();
+            }
+        });
+    });
 });
