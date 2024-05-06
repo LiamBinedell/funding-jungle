f661706bd51ba29623375a80ba9e4678fd1c9951
LiamBinedell <2574850@students.wits.ac.za>
2024-05-06 15:22:50 +0200

    quickest fix

--- src/api/routes/routerIndex.js
+++ src/api/routes/routerIndex.js
@@ -1,5 +1,4 @@
 const registerRouter = require('./registerRouter');
 const loginRouter = require('./loginRouter');
 const adminRouter = require('./adminRouter');
-const fundManagerRouter = require('./fundManagerRouter')
-module.exports = {registerRouter, loginRouter, adminRouter, fundManagerRouter};
+module.exports = {registerRouter, loginRouter, adminRouter};
