73fa9fbea703804a06aad7e23974edfa03e3d820
TebogoSeopa20 <160424099+TebogoSeopa20@users.noreply.github.com>
2024-05-17 07:09:57 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 //import routers
-const {registerRouter, loginRouter, adminRouter, fundManagerRouter} = require('./api/routes/routerIndex');
+const {registerRouter, loginRouter, adminRouter} = require('./api/routes/routerIndex');
 
 const app = express();
 
@@ -17,7 +17,6 @@ app.use(cors());
 app.use('/api/register', registerRouter);
 app.use('/api/admin', adminRouter);
 app.use('/api/login', loginRouter);
-app.use('/api/manage', fundManagerRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "components", "index.html"));
