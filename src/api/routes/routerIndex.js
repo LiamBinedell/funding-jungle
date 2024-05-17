740974240a425744f72e634fa19b926eae2e8966
TebogoSeopa20 <160424099+TebogoSeopa20@users.noreply.github.com>
2024-05-17 09:31:56 -0700

    Update routerIndex.js

--- src/api/routes/routerIndex.js
+++ src/api/routes/routerIndex.js
@@ -1,4 +1,4 @@
 const registerRouter = require('./registerRouter');
 const loginRouter = require('./loginRouter');
 const adminRouter = require('./adminRouter');
-module.exports = {registerRouter, loginRouter, adminRoute};
+module.exports = {registerRouter, loginRouter, adminRouter};
