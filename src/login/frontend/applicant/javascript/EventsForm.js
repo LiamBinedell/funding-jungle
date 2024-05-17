75f6fdfc6555fbd6f8ed20b999f69d1302d6ea19
TebogoSeopa20 <160424099+TebogoSeopa20@users.noreply.github.com>
2024-05-17 18:05:29 +0000

    Add files via upload

---
+++ src/login/frontend/applicant/javascript/EventsForm.js
@@ -0,0 +1,86 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCuTCXOYw--m1mg6F2q2zCp1gvf2uw6PYI",
+    authDomain: "simple-633fa.firebaseapp.com",
+    databaseURL: "https://simple-633fa-default-rtdb.firebaseio.com",
+    projectId: "simple-633fa",
+    storageBucket: "simple-633fa.appspot.com",
+    messagingSenderId: "616105900248",
+    appId: "1:616105900248:web:728ee133191dfba3f97fad",
+    measurementId: "G-LVEZE5P5F4"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Reference your database
+  var FormDB = firebase.database().ref("eventsform");
+  
+  document.getElementById("eventsForm").addEventListener("submit", submitForm);
+  
+  function submitForm(e) {
+    e.preventDefault();
+    var firstName = getElementVal("firstName");
+    var lastName = getElementVal("lastName");
+    var idNumber = getElementVal("idNumber");
+    var dob = getElementVal("dob");
+    var gender = getElementVal("gender");
+    var email = getElementVal("email");
+    var phone = getElementVal("phone");
+    var address1 = getElementVal("address1");
+    var location = getElementVal("institution");
+    var description = getElementVal("course");
+    var attendance = getElementVal("year");
+    var budget = getElementVal("grades");
+    var reason = getElementVal("reason");
+    var applicationId = generateUniqueId(); // Generate a unique application ID
+    var fundManagerEmail = new URLSearchParams(window.location.search).get('fundManagerEmail'); // Retrieve the funding manager's email from the URL parameter
+    var idDocument = document.getElementById("idDocument").files[0].name;
+    var poster = document.getElementById("parentId").files[0].name;
+    saveApplication(applicationId, firstName, lastName, idNumber, dob, gender, email, fundManagerEmail, phone, address1, location, attendance, budget, description, reason, idDocument, poster);
+  
+  // Display success message
+  const alertMessage = document.querySelector(".alert");
+  alertMessage.style.display = "block";
+  alertMessage.textContent = "Form submitted successfully.";
+
+  // Redirect to fetch.html after 1 seconds
+  setTimeout(() => {
+    window.location.href = "fetch.html";
+  }, 1000);
+  
+  // Reset the form
+  document.getElementById("eventsForm").reset();
+}
+  
+  const saveApplication = (applicationId, firstName, lastName, idNumber, dob, gender, email, fundManagerEmail, phone, address1, location, attendance, budget, description, reason, idDocument, poster) => {
+    var newApplication = FormDB.push();
+  
+    newApplication.set({
+      applicationId: applicationId,
+      firstName: firstName,
+      lastName: lastName,
+      idNumber: idNumber,
+      dob: dob,
+      gender: gender,
+      email: email,
+      fundManagerEmail: fundManagerEmail,
+      phone: phone,
+      address1: address1,
+      location: location,
+      attendance: attendance,
+      budget: budget,
+      description: description,
+      reason: reason,
+      idDocument: idDocument,
+      poster: poster,
+    });
+  };
+  
+  const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  };
+
+  const generateUniqueId = () => {
+    return 'EVE-' + Math.random().toString(36).substr(2, 9).toUpperCase();
+  };
