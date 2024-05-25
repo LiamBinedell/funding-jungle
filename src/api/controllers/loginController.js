a79c241bd7af92cd9a4a9126a3f7591a30e42f30
LiamBinedell <2574850@students.wits.ac.za>
2024-05-25 12:49:08 +0200

    admin perms sorted

--- src/api/controllers/loginController.js
+++ src/api/controllers/loginController.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseDB = firebaseApp.initializeApp(firebaseConfig);
 const db = firestore.getFirestore(firebaseDB);
-const auth = authorization.getAuth();
+const auth = authorization.getAuth(firebaseDB);
 
 async function checkIfAccountActivated(email){
     const collectionRef = firestore.collection(db, "users");
@@ -32,29 +32,34 @@ async function checkIfAccountActivated(email){
 const loginController = async (req, res) => {
     const {email, pass} = req.body; 
 
-    const verificationStatus = await checkIfAccountActivated(email);
-    if (!verificationStatus)
-        res.status(401).send("Account pending verification. Please try again later");
-    else{
-        authorization.signInWithEmailAndPassword(auth, email, pass)
-        .then((userCredential) => {
-            const user = userCredential.user;
+    try {
+        const userCredential = await authorization.signInWithEmailAndPassword(auth, email, pass);
+        const user = userCredential.user;
+        const verificationStatus = await checkIfAccountActivated(email);
+
+        if (!verificationStatus){
+            await authorization.signOut(auth);
+
+            res.status(401).send("Account pending verification. Please try again later");
+        } else {
             console.log(`USER LOGIN: ${user.uid}: ${email}`);
 
             const documentRef = firestore.doc(db, "users", user.uid);
-            firestore.getDoc(documentRef)
-            .then(documentSnapshot => {
-                if (documentSnapshot.exists()){
-                    const role = documentSnapshot.data()["role"];
-                    res.status(200).send(role);
-                }
-            })
-            .catch(error => console.error("ERROR:", error));
-        })
-        .catch((error) => {
-            console.error("Error logging in", error);
-            res.status(401).send("Invalid email or password");
-        });
+
+            const documentSnapshot = await firestore.getDoc(documentRef);
+
+            if (documentSnapshot.exists()){
+                const role = documentSnapshot.data()["role"];
+                const result = {
+                    uid: user.uid,
+                    message: role
+                };
+                res.status(200).json(result);
+            }
+        }
+    } catch (e) {
+        console.error("ERROR:", e);
+        res.status(500).json({message: "Login error"});
     }
 };
 
--- src/frontend/login/javascript/login.js
+++ src/frontend/login/javascript/login.js
@@ -1,36 +1,42 @@
-async function login(emailAddr, password){
+if (sessionStorage.getItem('uid'))  
+  sessionStorage.removeItem('uid');
+
+async function login(emailAddr, password) {
   const postOptions = {
     method: "POST",
     headers: {
-        "Content-Type": "application/json"
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
-        email: emailAddr,
-        pass: password
+      email: emailAddr,
+      pass: password
     })
   };
 
   const data = await fetch('/api/login/', postOptions);
-  const response = await data.text();
+  const response = await data.json();
 
-  switch (response){
+  switch (response['message']) {
     case "applicant":
       alert('Applicant successfully logged in');
-     sessionStorage.setItem('loggedInApplicant', emailAddr);
+      sessionStorage.setItem('loggedInApplicant', emailAddr);
+      sessionStorage.setItem('uid', response['uid']);
       window.location.href = "../../applicant/html/Applicant.html";
       break;
     case "fundingManager":
       alert('Funding manager successfully logged in');
       // Store email address in sessionStorage
       sessionStorage.setItem('loggedInFundingManager', emailAddr);
+      sessionStorage.setItem('uid', response['uid']);
       window.location.href = "../../fundingManager/html/FundingManager.html";
       break;
     case "admin":
       alert('Admin successfully logged in');
+      sessionStorage.setItem('uid', response['uid']);
       window.location.href = "../../admin/html/adminDashboard.html";
       break;
     default:
-      alert(response);
+      alert(response['message']);
       break;
   }
 }
